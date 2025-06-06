a8360d20b7e702986378395511ce940831549e72
nobody1902 <nobody1902@proton.me>
2025-06-06 21:54:49 +0200

    Implemented website translations.

--- main.py
+++ main.py
@@ -1,34 +1,58 @@
 from flask import Flask, render_template, session, request, redirect, url_for
 import translation
+import os
 
-app = Flask("Test")
+app = Flask("Pifelprojekt")
+app.secret_key = os.environ.get("SECRET_KEY")
 
-def parse_lang(req):
-    l = req.headers.get("Accept-Language", "en").split(",")[0]
-    return l
+DEFAULT_LANG = os.environ.get("DEFAULT_LANG", "en")
+
 
 @app.route("/changelang")
 def changelang():
     lang = request.args.get("newlang", None)
     if not lang:
-        return session.get("lang", parse_lang(request))
-    
+        return session.get("lang", DEFAULT_LANG)
+
     session["lang"] = lang
     return redirect(request.referrer or url_for("index"))
+
 
 @app.route("/login/", methods=["GET"])
 def login():
     if "username" in session:
         return redirect(url_for("index"))
-    lang = session.get("lang", parse_lang(request))
-    return render_template("login.jinja", t=lambda x: translation.get(x, lang))
-    
+    lang = session.get("lang", DEFAULT_LANG)
+    return render_template(
+        "login.jinja", LANG=lang, t=lambda x: translation.get(x, lang)
+    )
+
+
+@app.route("/login/", methods=["POST"])
+def login_action():
+    if "username" in session:
+        return redirect(url_for("index"))
+
+    username = request.form.get("username", None)
+    password = request.form.get("password", None)
+
+    if not username or not password:
+        # TODO: Show error
+        return redirect(url_for("login"))
+
+    # TODO: Implement a login system
+    print(username, password)
+
+    return redirect(url_for("index"))
 
 
 @app.route("/")
 def index():
-    lang = session.get("lang", parse_lang(request))
-    return render_template("index.jinja", t=lambda x: translation.get(x, lang))
+    lang = session.get("lang", DEFAULT_LANG)
+    return render_template(
+        "index.jinja", LANG=lang, t=lambda x: translation.get(x, lang)
+    )
+
 
 if __name__ == "__main__":
     translation.load()
--- translation.py
+++ translation.py
@@ -3,12 +3,18 @@ import codecs
 
 translations = {}
 
+
 def load():
     global translations
     dotenv.load_dotenv()
-    with codecs.open(os.environ.get("TRANSLATION_FILE", "translation.json"), encoding="utf-8") as f:
+    with codecs.open(
+        os.environ.get("TRANSLATION_FILE", "translation.json"), encoding="utf-8"
+    ) as f:
         translations = json.loads(f.read())
-    
+
+
 def get(key: str, lang: str) -> str:
     load()
+    if lang not in translations:
+        return translations["sl"][key]
     return translations[lang][key]
